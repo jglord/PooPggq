c4df856e11c5d50c29bd7e9fb216ed856ba4f55c
jglord <joao123goulart@gmail.com>
2018-08-10 17:28:07 -0300

    JsStudy

--- estudos_alternativos/JS/eloquent_javascript/teste.js
+++ estudos_alternativos/JS/eloquent_javascript/teste.js
@@ -1,5 +1,88 @@
 
-console.log(countChar("kakkerlak","k"));
+
+
+var number = 123;
+
+console.log(parseInt(number.toString().split('').reverse().join('')));
+
+
+
+
+
+
+var array = [
+    { name: "Itagores Lira", mother: "Ionara", 
+      sexo: "m",
+      curso: "Ciência da computação"},
+    { name: "João Goulart", mother: "Laurita",
+      sexo: "m",
+      curso: "Ciência da computação"}
+]
+
+function map(array, transform) {
+    var mapped = [];
+    for(var i = 0; i < array.length; i++) {
+        mapped.push(transform(array[i]))
+    }
+    return mapped;
+}
+
+/*var names = filter(array, (person) => {
+    return person.name;
+});
+
+console.log(map(names, (person) => {
+    return person.name;
+}));*/
+
+
+function filter(array, test) {
+    var okay = [];
+    for(var i = 0; i < array.length; i++) {
+        if(test(array[i])){
+            okay.push(array[i]);
+        }
+    }
+    return okay;
+}
+//
+//console.log(array.filter((person) => {
+//    return person.mother === "Laurita";
+//}));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var numbers = [1, 2, 3, 4, 5], sum = 0;
+numbers.forEach(function(number) {
+  sum += number;
+});
+//console.log(sum);
+
+function greaterThan(n) {
+    return function(m) { return m > n; };
+}
+
+var greaterThan10 = greaterThan(10);
+//console.log(greaterThan10(11));
+// → true
+
 
 
 function isEven(n) {

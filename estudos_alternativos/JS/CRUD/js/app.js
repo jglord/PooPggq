841c03ceebe7eef78129550c791526b42fcc2504
jglord <joao123goulart@gmail.com>
2018-08-13 08:07:14 -0300

    Crud

---
+++ estudos_alternativos/JS/CRUD/js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBUICvdS99aCVwHuHon245w0NGMBEKkw2w",
+    authDomain: "crud-basico-48f9b.firebaseapp.com",
+    databaseURL: "https://crud-basico-48f9b.firebaseio.com",
+    projectId: "crud-basico-48f9b",
+    storageBucket: "crud-basico-48f9b.appspot.com",
+    messagingSenderId: "222022413789"
+};
+firebase.initializeApp(config);
--- estudos_alternativos/JS/CRUD/js/real-time-database.js
+++ estudos_alternativos/JS/CRUD/js/real-time-database.js
@@ -1,8 +1,9 @@
+var usersList = document.getElementById('usersList');
 var nameInput = document.getElementById('nameInput');
-var ageInput  = document.getElementById('ageInput');
+var ageInput = document.getElementById('ageInput');
 var addButton = document.getElementById('addButton');
 
-addButton.addEventListener('click', function() {
+addButton.addEventListener('click', () => {
     create(nameInput.value, ageInput.value);
 });
 
@@ -12,6 +13,41 @@ function create(name, age) {
         age: age
     };
 
-    console.log(data);
-
+    return firebase.database().ref().child('users').push(data);
 }
+
+/*
+    <tr class="table-primary">
+        <td>teste1</td>
+        <td>teste2</td>
+    </tr>
+
+    usersList.innerHTML = '';
+
+    var tr = document.createElement('tr');
+    tr.classList.add('table-primary');
+
+    var tdName = document.createElement('td').appendChild(document.createTextNode('NOME1'));;
+
+    var tdAge = document.createElement('td').appendChild(document.createTextNode('IDADE1'));;
+
+    tr.appendChild(tdName);
+    tr.appendChild(tdAge);
+
+    usersList.appendChild(tr);
+*/
+
+firebase.database().ref('users').on('value', (snapshot) => {
+    usersList.innerHTML = '';
+    var i = 1;
+    // Item vem do snapshot
+    snapshot.forEach( (item) => {
+        var li = document.createElement('li');
+        li.classList.add('list-group-item');
+        li.appendChild(document.createTextNode(i + ' | Nome: ' +  item.val().name + '  |  Idade: ' + item.val().age));
+        usersList.appendChild(li);
+        i++;
+        console.log(li);
+
+    });
+})
